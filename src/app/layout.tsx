f877a4cadd87e9ab3753161422a97f6c28f1350e
dayongkr <dayongkr@gmail.com>
2024-08-22 23:39:14 +0900

    feat: initialize temp layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 
 const pretendard = localFont({
-  src: "../fonts/PretendardVariable.woff2",
-  display: "swap",
+  src: '../fonts/PretendardVariable.woff2',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "이력서 - 이다용 (Dayong Lee)",
-  description: "안녕하세요, 프론트엔드 개발자 이다용입니다.",
+  title: '이력서 - 이다용 (Dayong Lee)',
+  description: '안녕하세요, 프론트엔드 개발자 이다용입니다.',
 };
 
 export default function RootLayout({
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={pretendard.className}>
-        <main>{children}</main>
+        <div className="flex items-center justify-center px-8 py-6 md:px-24 md:py-16">{children}</div>
       </body>
     </html>
   );
