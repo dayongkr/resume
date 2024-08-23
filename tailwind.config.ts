face3043ed5ff8fa05ace32e1090288f478ff6c7
dayongkr <dayongkr@gmail.com>
2024-08-23 21:22:22 +0900

    feat: edit bio section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,6 @@ const config = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
       colors: {
         border: 'hsl(var(--border))',
