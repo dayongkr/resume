ee24f672b9e4fd19df0adf0128c0bff5d86dffe4
dayongkr <dayongkr@gmail.com>
2024-08-25 16:02:43 +0900

    fix(bio): change contents and notice this is not finished

--- src/app/_components/BioSection.tsx
+++ src/app/_components/BioSection.tsx
@@ -1,18 +1,21 @@
-import { Section } from '@/components/Section';
+import { MainSection } from '@/components/MainSection';
 
 export function BioSection() {
   return (
-    <Section>
+    <MainSection>
+      <blockquote>🚧 작성 중으로 일부 내용은 정확하지 않을 수 있습니다.</blockquote>
       <h1>
         안녕하세요,
         <br />
-        프론트엔드 개발자 <span className="border-b-4 border-primary">이다용</span>입니다.
+        프론트엔드 개발자 <span className="underline decoration-primary underline-offset-8">이다용</span>
+        입니다.
       </h1>
-      <p className="text-lg !leading-relaxed md:text-2xl">
-        첫 회사를 찾고 있는 성균관대학교 소프트웨어학과 4학년 학부생이며, 사용자 경험을 향상하는 것을 좋아합니다.
-        사용자에는 동료 또는 개발자도 포함되며, 단순함이 사용자 경험에 중요하다고 생각합니다. 회사에 속한 개발자로서
-        회사의 목표를 우선시하며, 비용 대비 얻을 수 있는 가치를 고려하여 회사의 이익을 극대화할 것을 약속드립니다.
+      <p>
+        자바스크립트 그리고 자동화에 관심이 많은 프론트엔드 개발자이며, 사용자 중심으로 개발하는 것을 좋아합니다.
+        사용자에는 동료 또는 개발자도 포함되며, 중복 및 불필요한 일을 최소화한 단순함이 사용자 경험에 중요하다고
+        생각합니다. 최근에는 ECMAScript의 제안서와 명세서를 통해 동작 원리와 새로운 기능을 익히고 있으며, 오픈 소스
+        기여를 통해 학습한 내용을 활용하고 있습니다.
       </p>
-    </Section>
+    </MainSection>
   );
 }
