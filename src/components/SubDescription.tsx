230e3dd9bfeb2e8c76d4bd1db6ff4ffd7771341a
dayongkr <dayongkr@gmail.com>
2024-08-29 22:39:51 +0900

    fix(component): remove small property and change date.to property to optional

--- src/components/SubDescription.tsx
+++ src/components/SubDescription.tsx
@@ -6,16 +6,18 @@ function formatDateString(date: Date | 'current') {
   return date === 'current' ? '현재' : `${String(date.year).padStart(4, '20')}.${String(date.month).padStart(2, '0')}`;
 }
 
-export function SubDescription({ date, link }: Readonly<{ date?: { from: Date; to: Date | 'current' }; link?: Link }>) {
+export function SubDescription({
+  date,
+  link,
+}: Readonly<{ date?: { from: Date; to?: Date | 'current' }; link?: Link }>) {
   return (
     <p className="not-prose text-sm opacity-80 md:text-base">
-      {date && `${formatDateString(date.from)} ~ ${formatDateString(date.to)}`}
+      {date && `${formatDateString(date.from)}`}
+      {date && date.to && ` ~ ${formatDateString(date.to)}`}
       {link && (
         <>
           {' / '}
-          <LinkBlank href={link.href} small>
-            {link.title}
-          </LinkBlank>
+          <LinkBlank href={link.href}>{link.title}</LinkBlank>
         </>
       )}
     </p>
