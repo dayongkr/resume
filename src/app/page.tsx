f89a46f607a44f42c98ec14397e556ce2029e068
dayongkr <dayongkr@gmail.com>
2024-08-31 22:33:33 +0900

    fix(layout): remove footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,6 @@ export default function Home() {
       <AwardSection />
       <CertificationSection />
       <ContactSection />
-      <blockquote className="text-base">
-        더 궁금하신 부분이 있으시다면, 상단 연락처로 연락주시면 감사하겠습니다.
-      </blockquote>
     </main>
   );
 }
