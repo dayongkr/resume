620340acf1daf336267775f6462023a9a9788aa9
dayongkr <dayongkr@gmail.com>
2024-08-31 21:16:10 +0900

    fix: add margin

--- src/app/_components/CertificationSection.tsx
+++ src/app/_components/CertificationSection.tsx
@@ -1,17 +1,20 @@
 import { List } from '@/components/List';
 import { MainSection } from '@/components/MainSection';
+import { SubSection } from '@/components/SubSection';
 
 export function CertificationSection() {
   return (
     <MainSection title="자격증">
-      <List
-        items={[
-          { children: 'OPIc(English) IH - 2024.03' },
-          { children: '삼성전자 DX S/W 역량테스트 B형(C++) - 2024.02' },
-          { children: '정보처리산업기사 - 2022.09' },
-          { children: '네트워크관리사 2급 - 2022.07' },
-        ]}
-      />
+      <SubSection>
+        <List
+          items={[
+            { children: 'OPIc(English) IH - 2024.03' },
+            { children: '삼성전자 DX S/W 역량테스트 B형(C++) - 2024.02' },
+            { children: '정보처리산업기사 - 2022.09' },
+            { children: '네트워크관리사 2급 - 2022.07' },
+          ]}
+        />
+      </SubSection>
     </MainSection>
   );
 }
