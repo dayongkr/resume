a78d910e4f840e24e6d8196fe42516a4276c24b5
dayongkr <dayongkr@gmail.com>
2024-09-01 20:09:24 +0900

    fix: change print banner content

--- src/app/_components/PrintBanner.tsx
+++ src/app/_components/PrintBanner.tsx
@@ -9,8 +9,8 @@ export function PrintBanner() {
   return (
     isPrint && (
       <blockquote className="absolute top-0">
-        이 이력서의 최신 버전은 <LinkBlank href="https://dayongkr.github.io">웹 사이트</LinkBlank>에서 확인하실 수
-        있습니다.
+        이력서를 검토해 주셔서 감사드리며, <LinkBlank href="https://dayongkr.github.io">dayongkr.github.io</LinkBlank>
+        에서도 확인하실 수 있습니다.
       </blockquote>
     )
   );
