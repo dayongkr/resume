addc546df14e0618a4ca6a2363fc06b64c960b58
dayongkr <dayongkr@gmail.com>
2024-09-01 15:43:36 +0900

    fix(education): update contents

--- src/app/_components/EducationSection.tsx
+++ src/app/_components/EducationSection.tsx
@@ -6,20 +6,20 @@ import { SubSection } from '@/components/SubSection';
 export function EducationSection() {
   return (
     <MainSection title="교육">
-      <SubSection title={{ text: '성균관대학교 소프트웨어학과', sub: { text: '4.09 / 4.50' } }}>
+      <SubSection title={{ text: '성균관대학교 소프트웨어학과', sub: { text: '4.09 / 4.5' } }}>
         <SubDescription date={{ from: { year: 2023, month: 3 }, to: { year: 2025, month: 2 } }} />
         <List
           items={[
             {
               children:
-                'CS 지식에 대한 열망과 대학 진학을 제대로 준비하지 못한 아쉬움으로, 전역 후 약 6개월간 독학하여 편입하였습니다.',
+                'CS 지식에 대한 열망과 대학 진학을 제대로 준비해보지 못한 아쉬움으로, 전역 후 약 6개월간 독학하여 편입하였습니다.',
             },
             {
               children: '첫 학기에 개발 동아리에 참여하고 성적 우수상을 받는 등 새로운 환경에 빠르게 적응했습니다.',
             },
             {
               children:
-                'xv6 운영체제와 리눅스 네트워크 모듈 개선 등 다양한 과제를 수행하고, 인공지능과 기계학습 관련 수업을 들으며 컴퓨터 공학 전반에 대한 기본 지식을 쌓았습니다.',
+                'xv6 운영체제와 리눅스 네트워크 모듈 개선 등 다양한 과제를 수행하고, 기계 학습과 HCI 등의 수업을 들으며 컴퓨터 공학 전반에 대한 기본 지식을 쌓았습니다.',
             },
           ]}
         />
