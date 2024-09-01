9b282be05e2e7ee456b83a50d82836d27b834f30
dayongkr <dayongkr@gmail.com>
2024-09-01 15:56:43 +0900

    fix(certification): update contents

--- src/app/_components/CertificationSection.tsx
+++ src/app/_components/CertificationSection.tsx
@@ -8,8 +8,8 @@ export function CertificationSection() {
       <SubSection>
         <List
           items={[
-            { children: 'OPIc(English) IH - 2024.03' },
-            { children: '삼성전자 DX S/W 역량테스트 B형(C++) - 2024.02' },
+            { children: '오픽 IH (영어) - 2024.03' },
+            { children: '삼성전자 SW 역량테스트 B형 (C++) - 2024.02' },
             { children: '정보처리산업기사 - 2022.09' },
             { children: '네트워크관리사 2급 - 2022.07' },
           ]}
