ffae08888a551e8cecc55cb1d4472609ac0cd0d0
dayongkr <dayongkr@gmail.com>
2024-09-19 19:47:21 +0900

    fix(activity): update content and remove redundant details in activity section

--- src/app/_components/ActivitySection.tsx
+++ src/app/_components/ActivitySection.tsx
@@ -12,7 +12,7 @@ export function ActivitySection() {
           items={[
             {
               children:
-                'ECMAScript 7 이후의 제안서와 명세서를 읽고, 이를 한국인 개발자들에게 쉽게 전달하기 위해 깃허브 리포지토리에 정리 및 요약한 글을 작성하고 있습니다.',
+                'ECMAScript 7 이후의 제안서와 명세서를 읽고, 이를 한국인 개발자에게 쉽게 전달하기 위해 깃허브 리포지토리에 정리 및 요약한 글을 작성하고 있습니다.',
               link: {
                 title: '해당 리포지토리',
                 href: 'https://github.com/dayongkr/ecmascript-summary',
@@ -47,15 +47,11 @@ export function ActivitySection() {
         <List
           items={[
             {
-              children:
-                '교내 개발 동아리 중 기술적으로 가장 활발한 성균관대학교 코딩 플랫폼 개발 동아리에서 프론트엔드 팀장과 멘토로 활동했습니다.',
+              children: '교내 개발 동아리 중 기술적으로 가장 활발한 스꾸딩에서 프론트엔드 팀장과 멘토로 활동했습니다.',
             },
             {
               children:
                 '배포 지연으로 기존 부원들이 이탈하는 상황에서도 열심히 활동하여, 현재는 베타 테스트를 성공적으로 마쳤으며, 활동 인원도 약 10명에서 28명으로 성장하는 과정을 함께 했습니다.',
-            },
-            {
-              children: '기술 관련 새로운 소식을 주기적으로 공유하여 팀에 기술적인 활력을 불어넣었습니다.',
             },
             {
               children:
